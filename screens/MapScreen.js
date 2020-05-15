da03fdf625b6fa41d20cf2ecbfed3ce38ea50d9f
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2020-05-14 18:05:18 -0700

    map updated

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,42 +1,4 @@
-
 import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
-import { MonoText } from '../components/StyledText';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Button } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-
-export default class App extends React.Component {
-
-  render(){
-    return (
-
-      <View style={styles.container}>
-      <Text>This is where the map is.</Text>
-
-  
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightgreen',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
-
-
-
-
-
-/*import * as WebBrowser from 'expo-web-browser';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import React, {useState, useEffect} from 'react';
@@ -196,4 +158,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#2e78b7',
   },
-});*/
+});
