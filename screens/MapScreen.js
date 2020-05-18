77e751cbf75adbcba2c893deb23b9c9da1c3c43c
Martin-Wilson <martinwilson450@gmail.com>
2020-05-18 17:17:56 -0500

    Markers added

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,7 +2,7 @@ import * as WebBrowser from 'expo-web-browser';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker} from 'react-native-maps';
 import { Image, Platform, StyleSheet, Text, Dimensions, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
@@ -10,16 +10,29 @@ import { MonoText } from '../components/StyledText';
 import { render } from 'react-dom';
 
 export default class MapScreen extends React.Component {
-state = {
-  region: {
-    latitude: 43.080330,
-    longitude: -88.075455,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0412
-  },
-  userlatitude: 0,
-  userlongitude: 0
+
+constructor(){
+  super();
+  this.state = {
+    region: {
+      latitude: 43.080330,
+      longitude: -88.075455,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0412
+    },
+    userlatitude: 0,
+    userlongitude: 0,
+    markers: [
+    {id: "1", LatLng: {latitude: 43.086364, longitude:-88.070863}},
+    {id: "2", LatLng: {latitude: 43.085612, longitude:-88.066721}},
+    {id: "3", LatLng: {latitude: 43.075660, longitude:-88.066142}},
+    {id: "4", LatLng: {latitude: 43.075252, longitude:-88.068459}},
+    {id: "5", LatLng: {latitude: 43.079756, longitude:-88.087813}}
+  ]
+  
+  }
 }
+
 
 getLocation = async () =>{
   let {status} = await Permissions.askAsync(Permissions.LOCATION);
@@ -31,6 +44,7 @@ getLocation = async () =>{
   this.setState({userlatitude: location.coords.latitude});
   this.setState({userlongitude: location.coords.longitude}); 
 }
+
 
 onComponentWillMount(){
   this.getLocation();
@@ -44,6 +58,14 @@ render(){
       region = {this.state.region}
       onRegionChangeComplete = {(region) =>this.setState({region})}
       >
+      {this.state.markers.map((marker) => {
+          return <Marker
+            coordinate={marker.LatLng}
+            title={marker.id}
+          >
+
+          </Marker>
+      })}
 
       </MapView>
     </View>
