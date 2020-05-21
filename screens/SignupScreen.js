4028c1511c9c912595f31c956bba2c836ce6f446
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2020-05-20 18:26:24 -0700

    Signup addons...

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -10,13 +10,51 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 
 export default class App extends React.Component {
+    state={
+        email:"",
+        password:"",
+        vendor:"",
+
+      }
 
   render(){
     return (
 
       <View style={styles.container}>
+     <Text>Enter your information to create an account.</Text>
 
-     <Text>Sign up here!</Text>
+     
+     <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Password" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Confirm Password" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Vendor" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
 
   
       </View>
@@ -30,5 +68,18 @@ const styles = StyleSheet.create({
       backgroundColor: 'lightgreen',
       alignItems: 'center',
       justifyContent: 'center',
-    }
+    },
+    inputText:{
+        height:50,
+        color:"white"
+      },
+      inputView:{
+        width:"80%",
+        backgroundColor:"grey",
+        borderRadius:25,
+        height:50,
+        marginBottom:20,
+        justifyContent:"center",
+        padding:20
+      }
 });
